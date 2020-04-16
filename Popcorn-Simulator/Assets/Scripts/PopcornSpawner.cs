9cb9fcfb49f231cb7b6755ddca77451d18a22790
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2020-04-16 16:09:54 +0200

    popcornArray i PopcornSpawner

--- Popcorn-Simulator/Assets/Scripts/PopcornSpawner.cs
+++ Popcorn-Simulator/Assets/Scripts/PopcornSpawner.cs
@@ -6,6 +6,7 @@ public class PopcornSpawner : MonoBehaviour
 {
     public GameObject stekpanna;
     public GameObject popcorn;
+    public GameObject[] popcornArray;
 
     public float xPos;
     public float yPos;
@@ -13,16 +14,18 @@ public class PopcornSpawner : MonoBehaviour
     private float popcornSpawnNumber;
     private int randomAngle;
     private float panRadius = 0.4f;
+    private int randomPopcorn;
 
     private void FixedUpdate()
     {
+        randomPopcorn = Random.Range(0, popcornArray.Length);
         xPos = Random.Range(stekpanna.transform.position.x - panRadius, stekpanna.transform.position.x + panRadius);
         yPos = transform.position.y;
         randomAngle = Random.Range(-45, 45);
         
         popcornSpawnNumber = Random.Range(1, 10) / Stekpanna.stekpannaInstance.distanceToFire;
         if (popcornSpawnNumber > 9)
-            Instantiate(popcorn, new Vector3(xPos, yPos, 14f), Quaternion.AngleAxis(randomAngle, Vector3.one));
+            Instantiate(popcornArray [randomPopcorn], new Vector3(xPos, yPos, 15f), Quaternion.AngleAxis(randomAngle, Vector3.one));
     }
 
 
