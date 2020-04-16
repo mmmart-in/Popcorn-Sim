30438e50e73532f498dcb051b2dc5194441b39ee
henrysmitt <rickardsmitt@gmail.com>
2020-04-16 16:47:24 +0200

    Nu funkar det!

--- Popcorn-Simulator/Assets/Scripts/Stekpannan/Stekpanna.cs
+++ Popcorn-Simulator/Assets/Scripts/Stekpannan/Stekpanna.cs
@@ -8,6 +8,7 @@ public class Stekpanna : MonoBehaviour
     public Transform startPosition;
     public Transform mid;
     public float distanceToFire;
+    public float moveSpeed = 10;
     public bool canPop;
     public GameObject pan;
     public GameObject fire;
@@ -22,7 +23,7 @@ public class Stekpanna : MonoBehaviour
     {
         stekpannaInstance = this;
         canPop = false;
-        transform.position = startPosition.position;
+        //transform.position = startPosition.position;
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
@@ -59,30 +60,34 @@ public class Stekpanna : MonoBehaviour
         if (Camera.main.ScreenToWorldPoint(touch.position).x >= mid.position.x)
         {
             sr.flipX = false;
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            transform.position = new Vector3(touchPosition.x - panAdjustRightHand, touchPosition.y);
-            touchPosition.z = 15;
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position) - transform.position;
+            rb.velocity = new Vector3(touchPosition.x * moveSpeed, touchPosition.y * moveSpeed, 15);
+            if(transform.position.x - touchPosition.x < 0.5f)
+            {
+                rb.velocity = new Vector3(0, 0, 15);
+            }
+            //touchPosition.z = 15;
         }
         else if (Camera.main.ScreenToWorldPoint(touch.position).x < mid.position.x)
         {
             
             sr.flipX = true;
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            transform.position = new Vector3(touchPosition.x + panAdjustLeftHand, touchPosition.y);
-            touchPosition.z = 15;
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position) - transform.position;
+            rb.velocity = new Vector3(touchPosition.x * moveSpeed, touchPosition.y * moveSpeed, 15);
+            if (transform.position.x - touchPosition.x < 0.5f)
+            {
+                rb.velocity = new Vector3(0, 0, 15);
+            }
 
         }
     }
 
     void GrabWithMouse()
     {
-        if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x >= mid.position.x)
-        {
-            sr.flipX = false;
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(touchPosition.x - panAdjustRightHand, touchPosition.y, 15);
-        }
-        else if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x < mid.position.x)
+        
+        Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        rb.velocity = new Vector3(touchPosition.x * moveSpeed, touchPosition.y * moveSpeed, 15);
+        /*else if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x < mid.position.x)
         {
 
             sr.flipX = true;
@@ -90,6 +95,14 @@ public class Stekpanna : MonoBehaviour
             transform.position = new Vector3(touchPosition.x + panAdjustLeftHand, touchPosition.y, 15);
 
 
+        }*/
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Finish"))
+        {
+            print("Vägg");
         }
     }
 }
