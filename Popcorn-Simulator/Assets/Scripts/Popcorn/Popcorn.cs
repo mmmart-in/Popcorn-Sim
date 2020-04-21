e86c6c50cde75e78210b0fb2773ffd2579026ab6
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2020-04-21 14:37:19 +0200

    Revert "tog bort skräp"
    This reverts commit 612c3200ae76bf5f73a65f2ab8b26300542489c8.

--- Popcorn-Simulator/Assets/Scripts/Popcorn/Popcorn.cs
+++ Popcorn-Simulator/Assets/Scripts/Popcorn/Popcorn.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Popcorn : MonoBehaviour
 {   
-    private float upwards;
+    private float upwardsForce;
     private Rigidbody2D rb2d;
     
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        upwards = Random.Range(10, 15);
-        rb2d.AddForce(new Vector2(Random.Range(-2, 2), upwards), ForceMode2D.Impulse);
+        upwardsForce = Random.Range(10, 15);
+        rb2d.AddForce(new Vector2(Random.Range(-2, 2), upwardsForce), ForceMode2D.Impulse);
         
     }
 
