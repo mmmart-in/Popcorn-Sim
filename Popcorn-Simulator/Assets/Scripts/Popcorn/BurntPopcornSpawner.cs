beaaf1698afa6df446fae2c2e369e5efae9f0d82
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2020-04-23 15:40:34 +0200

    BurntPopcorn

---
+++ Popcorn-Simulator/Assets/Scripts/Popcorn/BurntPopcornSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurntPopcornSpawner : MonoBehaviour
+{
+    public static BurntPopcornSpawner burntInstance;
+    public float heat;
+    public bool closeToFire;
+
+
+    private void Start()
+    {
+        burntInstance = this; 
+        heat = 0f;
+    }
+    void FixedUpdate()
+    {
+        if (!closeToFire && heat > 0.41)
+            heat -= 0.4f * Time.fixedDeltaTime;
+        else if(closeToFire && heat <= 5)
+            heat += 0.2f * Time.fixedDeltaTime;       
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("HeatZone"))
+            closeToFire = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("HeatZone"))
+            closeToFire = false;
+    }
+    public float getHeat() {
+        return heat;
+    }
+}
--- Popcorn-Simulator/Assets/Scripts/Popcorn/PopcornSpawner.cs
+++ Popcorn-Simulator/Assets/Scripts/Popcorn/PopcornSpawner.cs
@@ -6,6 +6,7 @@ public class PopcornSpawner : MonoBehaviour
 {
     public GameObject stekpanna;
     public GameObject popcorn;
+    public GameObject burntPopcorn;
 
     public float xPos;
     public float yPos;
@@ -19,11 +20,15 @@ public class PopcornSpawner : MonoBehaviour
         xPos = Random.Range(stekpanna.transform.position.x - panRadius, stekpanna.transform.position.x + panRadius);
         yPos = transform.position.y;
         randomAngle = Random.Range(-45, 45);
-        
-        popcornSpawnNumber = Random.Range(1, 10) / Stekpanna.stekpannaInstance.distanceToFire;
-        if (popcornSpawnNumber > 9)
-            Instantiate(popcorn, new Vector3(xPos, yPos, 14f), Quaternion.AngleAxis(randomAngle, Vector3.one));
+
+        popcornSpawnNumber = Random.Range(2, 20) * Stekpanna.stekpannaInstance.distanceToFire;
+        if (popcornSpawnNumber < 3)
+            if (BurntPopcornSpawner.burntInstance.getHeat() * Random.Range(1, 5) > 10)
+                Instantiate(burntPopcorn, new Vector3(xPos, yPos, 14f), Quaternion.AngleAxis(randomAngle, Vector3.one));
+            else
+                Instantiate(popcorn, new Vector3(xPos, yPos, 14f), Quaternion.AngleAxis(randomAngle, Vector3.one));
     }
+
 
 
 }
