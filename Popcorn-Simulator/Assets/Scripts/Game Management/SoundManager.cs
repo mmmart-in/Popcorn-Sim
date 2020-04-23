ee42917b51e2f6526756ed185797be89952a2786
henrysmitt <rickardsmitt@gmail.com>
2020-04-23 18:52:17 +0200

    ljud

---
+++ Popcorn-Simulator/Assets/Scripts/Game Management/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static AudioClip popcorn, wind, burnedPopcorn, goldPopcorn;
+    static AudioSource audSrc;
+
+    void Start()
+    {
+        audSrc = GetComponent<AudioSource>();
+
+        popcorn = Resources.Load<AudioClip>("popcorn");
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "popcorn":
+                //audSrc.pitch = Random.Range(0.8f, 1f);
+                audSrc.PlayOneShot(popcorn, 0.5f);
+                break;
+
+        }
+    }
+}
