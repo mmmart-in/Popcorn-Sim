a37d8fbad20801140fc2910a97403d24d71c4562
henrysmitt <rickardsmitt@gmail.com>
2020-04-27 14:51:11 +0200

    Timer på gång. Test för merge med master.

--- Popcorn-Simulator/Assets/Scripts/Game Management/GameController.cs
+++ Popcorn-Simulator/Assets/Scripts/Game Management/GameController.cs
@@ -2,19 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
 
     public int cornCounterThisLevel;
     public Text cornCounterText;
 
-    /*public GameObject eld;
-    public GameObject popcorn;
-    public GameObject stekpanna;
+    [Header("Timer")]
+    public float timer = 60;
+    
 
-    public float distance;
-    private float randomAngle;
-    private float popcornSpawnNumber;*/
+    
     void Start()
     {
 
@@ -25,26 +24,26 @@ public class GameController : MonoBehaviour
     {
         cornCounterText.text = "" + cornCounterThisLevel;
 
-        /*distance = Vector3.Distance(stekpanna.transform.position, eld.transform.position);
-
-        randomAngle = Random.Range(-45, 45);
-
-        if (Input.GetKeyDown(KeyCode.B))
-            Instantiate(popcorn, new Vector3(PopcornSpawner.popcornSpawnerInstance.xPos, PopcornSpawner.popcornSpawnerInstance.yPos, 0f), Quaternion.AngleAxis(randomAngle, Vector3.one));*/
-
-
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            TimeRanOut();
+        }
     }
     private void FixedUpdate()
     {
-        /*popcornSpawnNumber = Random.Range(1, 10) / distance;
-        if(popcornSpawnNumber > 9)
-            Instantiate(popcorn, new Vector3(PopcornSpawner.popcornSpawnerInstance.xPos, PopcornSpawner.popcornSpawnerInstance.yPos, 0f), Quaternion.AngleAxis(randomAngle, Vector3.one));*/
+        
 
     }
 
     public void CatchPopcorn()
     {
         cornCounterThisLevel++;
+    }
+
+    private void TimeRanOut()
+    {
+
     }
 
 
