8581acd433ef13347b64ce29800be712e41e8097
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2020-04-30 13:10:42 +0200

    SaveBuild

--- Popcorn-Simulator/Assets/Scripts/Game Management/GameControl.cs
+++ Popcorn-Simulator/Assets/Scripts/Game Management/GameControl.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl gameControl;
     public int popcornCount;
+    public GameObject[] scenarios;
+    public bool[] scenariosUnlocked;
+    
+
 
     void Awake()
     {
@@ -19,17 +25,36 @@ public class GameControl : MonoBehaviour
 
         Load();
     }
-        public void updatePopcornCount(int popcorn) {
+       
+    public void updatePopcornCount(int popcorn) {
         popcornCount += popcorn;
         Save();
     }
 
+    public void Unlock(int scenarioNum) {
+        scenariosUnlocked[scenarioNum] = true;
+        Debug.Log(scenariosUnlocked[1]);
+        Save();
+        Load();
+    }
+
     public void Save() {
-        SaveSystem.SaveData(new SaveDataContainer(popcornCount));
+        SaveSystem.SaveData(new SaveDataContainer(popcornCount, scenariosUnlocked));
     }
     public void Load()
     {
         popcornCount = SaveSystem.LoadPlayer().playerCurrency;
+
+        if (SaveSystem.LoadPlayer().scenariosUnlocked != null)
+            scenariosUnlocked = SaveSystem.LoadPlayer().scenariosUnlocked;
+        else
+            scenariosUnlocked = new bool[] { true, false, false };
+
+        for (int i = 0; i < scenarios.Length; i++ ) {
+            scenarios[i].GetComponent<Button>().interactable = scenariosUnlocked[i];
+            scenarios[i].transform.GetChild(0).GetComponent<Image>().enabled = !scenariosUnlocked[i];
+            }
+
         Debug.Log("loaded data " + SaveSystem.LoadPlayer().playerCurrency + " to gamecontrol, counter is " + popcornCount);
     }
 }
--- Popcorn-Simulator/Assets/Scripts/Game Management/SaveDataContainer.cs
+++ Popcorn-Simulator/Assets/Scripts/Game Management/SaveDataContainer.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class SaveDataContainer
 {
     public int currency;
+    public bool [] scenariosUnlocked;
 
-    public SaveDataContainer(int currency) {
-        this.currency = currency; 
+    public SaveDataContainer(int currency, bool [] scenariosUnlocked) {
+        this.currency = currency;
+        if(scenariosUnlocked != null)
+            this.scenariosUnlocked = scenariosUnlocked;
     }
 }
