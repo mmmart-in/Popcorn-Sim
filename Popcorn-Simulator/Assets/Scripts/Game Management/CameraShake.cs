81b7550505fb8fd674baafa4cb48eb5e1b15532a
henrysmitt <rickardsmitt@gmail.com>
2020-05-18 12:42:16 +0200

    fix med buggar
    fixade en bugg där kameran hamnade på fel plats

--- Popcorn-Simulator/Assets/Scripts/Game Management/CameraShake.cs
+++ Popcorn-Simulator/Assets/Scripts/Game Management/CameraShake.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraShake : MonoBehaviour
+
+    
+
 {
     public IEnumerator Shake (float duration, float magnitude)
     {
+
         Vector3 originalPos = transform.position;
 
         float timer = 0.0f;
@@ -16,7 +20,7 @@ public class CameraShake : MonoBehaviour
             float x = Random.Range(-1, 1) * magnitude;
             float y = Random.Range(-1, 1) * magnitude;
 
-            transform.localPosition = new Vector3(x, y, originalPos.z);
+            transform.localPosition = new Vector3(x, y, 0);
 
             yield return null;
         }
