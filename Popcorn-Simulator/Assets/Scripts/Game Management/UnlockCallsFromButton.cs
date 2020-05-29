d557ae15c6f4397369a962865564b8b3dde47e9f
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2020-05-29 09:10:51 +0200

    BoxCollider spisplatta, metod för moon-scene att kalla

--- Popcorn-Simulator/Assets/Scripts/Game Management/UnlockCallsFromButton.cs
+++ Popcorn-Simulator/Assets/Scripts/Game Management/UnlockCallsFromButton.cs
@@ -11,5 +11,7 @@ public class UnlockCallsFromButton : MonoBehaviour
     public void UnlockBeach() {
         GameControl.gameControl.Purchase(300, 2);
     }
-    public void Hey() { Debug.Log("Hey"); }
+    public void UnlockMoon() {
+        GameControl.gameControl.Purchase(450, 3);
+    }
 }
